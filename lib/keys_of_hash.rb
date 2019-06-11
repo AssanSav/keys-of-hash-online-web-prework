d5ec31a507215648f74c4dde02d9c331ca7e4c2d
assane savadogo <assane.savadogo81@gmail.com>
2019-06-11 21:56:35 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,10 +1,10 @@
-
+animals =  {"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"} 
 
 
 class Hash
   def keys_of(*arguments)
 	animals = []
-		.each { |animal, place|
+		self.each { |animal, place|
 			if arguments.include?(place)
 				animals << animal
 				end
